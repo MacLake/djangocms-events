91cf2824362a4b4bcd9ed67f4b4c5d0aabda38c0
Jens-Erik Weber <Jens-Erik.Weber@passiv.de>
2023-01-03 16:09:11 +0100

    Include djangocms_events/semantic.min.js in template instead of webpack bundle
    Fomantic UI components can’t accessed when the JS file is just imported in the webpack bundle.

--- assets/src/index.js
+++ assets/src/index.js
@@ -1,9 +1,14 @@
 import $ from 'jquery'
 import 'htmx.org'
 import 'fomantic-ui/dist/semantic.css'
-import 'fomantic-ui/dist/semantic.js'
 
 window.jQuery = $;
 window.$ = $;
 window.htmx = require('htmx.org');
 
+$(".djangocms-event-modal-trigger").click(function () {
+    $("#djangocms-event-detail").modal({
+        onHidden: $("#djangocms-event-detail").html('')
+    }).modal("show")
+})
+$('.ui.accordion').accordion()
