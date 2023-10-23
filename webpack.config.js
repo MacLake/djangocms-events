0dbaa1ea806348f9433855948ca879dd71d9fc85
Jens-Erik Weber <Jens-Erik Weber Jens-Erik.Weber@passiv.de>
2023-10-23 12:39:54 +0200

    Build webpack with splitChunks

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: {
         index: './assets/src/index.js',
-        calendar:  './assets/src/calendar.js',
+        calendar: './assets/src/calendar.js',
     },
     module: {
         rules: [
@@ -17,6 +17,11 @@ module.exports = {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, './src/djangocms_events/static/djangocms_events/webpack'),
         clean: true,
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
     },
     mode: 'production',
 };
